15487398703944b0e0c170fe1075e9c8237ffafe
Daniel <mattosfacul@gmail.com>
2022-12-17 19:38:25 -0300

    Criado autenticação para acesso.

--- home.php
+++ home.php
@@ -1,3 +1,13 @@
+<?php
+  session_start();
+
+  if(!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'SIM'){
+    header('location: index.php?login=erro2');
+  }
+?>
+
+
+
 <html>
   <head>
     <meta charset="utf-8" />
